56f5e6a837a5f09792c756dcc2955ba18b0f5fc7
yanghao <hao.yhao@gmail.com>
2015-03-30 19:55:00 +0800

    代码测试

---
+++ cm4j-test/src/main/java/com/cm4j/test/thread/concurrent/coordination/countdown_barrier/Self_LockTest.java
@@ -0,0 +1,46 @@
+package com.cm4j.test.thread.concurrent.coordination.countdown_barrier;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * Created by yanghao on 2015/1/27.
+ */
+public class Self_LockTest {
+
+    private static class Lock extends AbstractQueuedSynchronizer{
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return compareAndSetState(0, 1);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            setState(0);
+            return true;
+        }
+
+        public void lock() {
+            acquire(0);
+    }
+
+        public void unlock() {
+            release(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Lock lock = new Lock();
+        
+        new Thread(){
+        	public void run() {
+        		lock.lock();
+        		System.out.println("thread lock...");
+        		lock.unlock();
+        	}
+        }.start();
+        
+        lock.lock();
+        System.out.println("main lock...");
+        lock.unlock();
+    }
+}
---
+++ cm4j-test/src/main/java/com/cm4j/test/thread/concurrent/future/CallableAndFuture.java
@@ -0,0 +1,79 @@
+package com.cm4j.test.thread.concurrent.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * Created by yanghao on 2015/1/24.
+ */
+public class CallableAndFuture {
+    // 与Runnable类似，不同在于Callable有返回值
+    private final static Callable<Integer> callable = () -> {
+        Thread.sleep(5000);// 可能做一些事情
+        return new Random().nextInt(100);
+    };
+
+    /**
+     * 1.单线程运行获得FutureTask
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void t1() throws Exception {
+        System.out.println("------------------------");
+        FutureTask<Integer> future = new FutureTask(callable);
+        // 运行线程
+        new Thread(future).start();
+        // 获取结果
+        System.out.println(future.get());
+
+    }
+
+    /**
+     * 2.线程池运行获得Future
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void t2() throws Exception {
+        System.out.println("------------------------");
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        // 运行线程池
+        Future<Integer> future = threadPool.submit(callable);
+        // 获取结果
+        System.out.println(future.get());
+        // 结果获取到才关闭线程池
+        threadPool.shutdown();
+    }
+
+    /**
+     * 3.执行多个带返回值的任务，并取得多个返回值
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    private static void t3() throws Exception {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        CompletionService<Integer> cs = new ExecutorCompletionService<Integer>(threadPool);
+
+        System.out.println("-----------------------");
+        for (int i = 0; i < 5; i++) {
+            final int taskID = i;
+            cs.submit(() -> {
+                Thread.sleep(5000);// 可能做一些事情
+                return taskID;
+            });
+        }
+        // 可能做一些事情
+        for (int i = 0; i < 5; i++) {
+            System.out.println(cs.take().get());
+        }
+
+        // 其实也可以不使用CompletionService，可以先创建一个装Future类型的集合，用Executor提交的任务返回值添加到集合中，
+        // 最后遍历集合取出数据，代码略。
+    }
+
+    public static void main(String[] args) throws Exception {
+        t3();
+    }
+}
